81139e069d3fdc13358eee141573b055a0007b60
Raj Hiray <raj.hiray@outlook.com>
2021-05-04 19:20:08 +0530

    Syncfusion UI

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Syncfusion.Blazor;
 
 namespace BlazorPages.Client
 {
@@ -14,7 +15,7 @@ namespace BlazorPages.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddSyncfusionBlazor();
             await builder.Build().RunAsync();
         }
     }
